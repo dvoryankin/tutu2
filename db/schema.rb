fcb05815021a44d5ed0a649b3c523a531a7b5f26
dvoryankin <dvoryankindm@gmail.com>
2019-07-03 11:55:15 +0500

    add raiway_station using scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_02_182511) do
+
+  create_table "railway_stations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
