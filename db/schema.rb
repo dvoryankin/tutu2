37f33f933cde72f51a9cea4a9450e938477099b6
dvoryankin <dvoryankindm@gmail.com>
2019-07-25 18:13:15 +0500

    add associations stations trains

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_110830) do
+ActiveRecord::Schema.define(version: 2019_07_25_111413) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_07_25_110830) do
     t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "railway_station_id"
+    t.index ["railway_station_id"], name: "index_trains_on_railway_station_id"
   end
 
 end
