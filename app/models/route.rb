6f67f700650845210126434474ee643602d0ad19
dvoryankin <dvoryankindm@gmail.com>
2019-08-12 16:16:56 +0500

    add relations train, stations, routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,7 @@
 class Route < ApplicationRecord
   validates :name, presence: true
+
+  # has_and_belongs_to_many :railway_stations
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rs1 = RailwayStation.create(title: 'Moscow')
+rs2 = RailwayStation.create(title: 'Omsk')
+rs3 = RailwayStation.create(title: 'Minsk')
+rs4 = RailwayStation.create(title: 'Riga')
+
+train1 = Train.create(number: 123, current_station: rs1)
+train2 = Train.create(number: 456, current_station: rs1)
+
+train3 = Train.create(number: 777, current_station: rs2)
+train4 = Train.create(number: 888, current_station: rs2)
+
+route1 = Route.create(name: 'Moscow - Omsk')
+route2 = Route.create(name: 'Minsk - Riga')
+
+rs1.routes << route1
+rs3.routes << route2
