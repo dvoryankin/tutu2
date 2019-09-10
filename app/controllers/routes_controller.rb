73f2a653572dedf7b26d7fccabc5c350a9a5a7fd
dvoryankin <dvoryankindm@gmail.com>
2019-09-10 17:19:08 +0500

    create edit partial form, fix train, route, station controllers

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -70,6 +70,6 @@ class RoutesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def route_params
-    params.fetch(:route, {})
+    params.require(:route).permit(:name)
   end
 end
